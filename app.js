0d00f185c9b5c39def8904ff6316d3c4a8a97f66
Max Shaw <maxdshaw@gmail.com>
2018-03-05 17:22:40 -0500

    Added some default stuff

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ app.controller('Controller', ['$scope', '$window', '$location', '$anchorScroll',
   function($scope, $window, $location, $anchorScroll) {
     this.title = "Hello World"
 
+    this.showButton = false
+    this.updateShowButton = function(show) {
+      this.showButton = show
+    }
 
+    this.addItem = function() {
+      let number = this.items.length + 1
+      this.items.push({
+        title: "Item " + number,
+        subtitle: "Item " + number + " Subtitle",
+      })
+    }
+
+    this.removeItem = function() {
+      this.items.splice(-1, 1)
+    }
+
+    this.items = [];
+    this.addItem();
+    this.addItem();
+    this.addItem();
 
   }
 ]);
